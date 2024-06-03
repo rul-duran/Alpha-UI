fb503a4e6a90e98afb60a84a58ab62f7dc9c8d1f
rul-duran <raulduran816@gmail.com>
2024-06-02 17:37:46 -0700

    feat: added eslint parser and setup alias

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vitePluginTesting from 'vite-plugin-testing';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), vitePluginTesting()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@views': path.resolve(__dirname, 'src/views'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
